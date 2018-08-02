bed2cfc6b0a64e7da0e0f64a5ba0e7c536e25ae2
Peter Altosaar <peter.altosaar@gmail.com>
2018-08-01 21:37:42 -0400

    Move around refactoring/Run keys and add bookmark keys

--- keymap.c
+++ keymap.c
@@ -156,20 +156,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* IntelliJ (Fn)
  * ,-----------------------------------------------------------------------------------.
- * |      | Close|      |  End |  Run |      |      |Usages|Prev_M|Outlin|OpenIn|  PW  |
+ * |      | Close|      |  End |Refctr|      |      |Usages|Prev_M|Outlin|  Run |  PW  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |Refctr|Symbol|      | Open |  VCS |Hierar| Prev |Next_M| Next |      |      |
+ * |      |      |Symbol|      |OpenIn|  VCS |Hierar| Prev |Next_M| Next |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |Run It|Create|      |      | File | Menu |      |      | Info |      |
+ * |      |GoToBM|Run It|Create|S.BkMk|G.BkMk| File | Menu |      |      | Info |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |    KP_0     |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
  
 [_INTJ] = {
-  {_______, LCTL(KC_F4), _______, LCTL(KC_F2), LSFT(LALT(KC_F10)), _______, _______, LALT(KC_F7), LALT(KC_UP), LCTL(KC_F12), LALT(KC_F1), PW },
-  {_______, LCTL(LALT(LSFT(KC_T))), LCTL(LALT(LSFT(KC_N))), _______, LALT(KC_F1), LALT(KC_GRV), LCTL(LALT(KC_H)), LCTL(LALT(KC_LEFT)), LALT(KC_DOWN), LCTL(LALT(KC_RIGHT)), _______, _______ },
-  {_______, _______, LCTL(LSFT(KC_F10)), LALT(KC_INS), _______, _______, LCTL(LSFT(KC_N)), LSFT(LALT(KC_SCLN)), _______, _______, LCTL(KC_F1), _______ },
+  {_______, LCTL(KC_F4), _______, LCTL(KC_F2), LCTL(LALT(LSFT(KC_T))), _______, _______, LALT(KC_F7), LALT(KC_UP), LCTL(KC_F12), LSFT(LALT(KC_F10)), PW },
+  {_______, _______, LCTL(LALT(LSFT(KC_N))), _______, LALT(KC_F1), LALT(KC_GRV), LCTL(LALT(KC_H)), LCTL(LALT(KC_LEFT)), LALT(KC_DOWN), LCTL(LALT(KC_RIGHT)), _______, _______ },
+  {_______, LSFT(KC_F11), LCTL(LSFT(KC_F10)), LALT(KC_INS), LCTL(KC_F11), KC_F11, LCTL(LSFT(KC_N)), LSFT(LALT(KC_SCLN)), _______, _______, LCTL(KC_F1), _______ },
   {_______, _______, _______, _______, _______, LCTL(LALT(KC_SCLN)), LCTL(LALT(KC_SCLN)), _______, _______, _______, _______, _______ }
 }
 
