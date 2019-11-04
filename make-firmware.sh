c6fbbef63491918bcdf8f44719054d44add05401
Peter Altosaar <peter.alrtosaar@gmail.com>
2019-11-04 15:57:15 -0500

    Update build script to take in password and delete keymap afterward

--- make-firmware.sh
+++ make-firmware.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Password:"
+read -s password
+
 cp keymap.c ../qmk_firmware/keyboards/planck/keymaps/peteraltosaar/
 cd ../qmk_firmware
+sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/planck/keymaps/peteraltosaar/keymap.c
 make planck/rev5:peteraltosaar
+rm keyboards/planck/keymaps/peteraltosaar/keymap.c
 cd -
