1730ac12c44d85471bc1acf284a8292b200dccca
Peter Altosaar <peter.altosaar@gmail.com>
2021-04-07 22:17:45 -0400

    Update script to record both dynamic strings

--- make-firmware.sh
+++ make-firmware.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "Password:"
-read -s password
+echo "String 1: "
+read -s string1
+
+echo "String 2: "
+read -s string2
 
 cp keymap.c ../qmk_firmware/keyboards/planck/keymaps/peteraltosaar/
 cd ../qmk_firmware
-sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/planck/keymaps/peteraltosaar/keymap.c
+sed -i '' 's/STRING1_PLACEHOLDER/'$string1'/g' keyboards/planck/keymaps/peteraltosaar/keymap.c
+sed -i '' 's/STRING2_PLACEHOLDER/'$string2'/g' keyboards/planck/keymaps/peteraltosaar/keymap.c
 make planck/rev5:peteraltosaar
 rm keyboards/planck/keymaps/peteraltosaar/keymap.c
 cd -
